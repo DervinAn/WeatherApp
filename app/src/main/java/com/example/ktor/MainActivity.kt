f8b1e96956921a6e18ee94bc893b07be9a056fe2
Anas_Dervin <102409151+Anas040@users.noreply.github.com>
2025-02-08 21:03:59 +0100

    Splash screen duration

--- app/src/main/java/com/example/ktor/MainActivity.kt
+++ app/src/main/java/com/example/ktor/MainActivity.kt
@@ -6,13 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.ktor.core.presentation.HomeScreen
 import com.example.ktor.ui.theme.KtorTheme
 
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                false
+            }
+        }
         enableEdgeToEdge()
         setContent {
             KtorTheme {
@@ -21,6 +28,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
--- app/src/main/java/com/example/ktor/core/presentation/HomeScreen.kt
+++ app/src/main/java/com/example/ktor/core/presentation/HomeScreen.kt
@@ -213,8 +213,3 @@ fun HomeScreen() {
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-private fun HomeScreenPreview() {
-//    HomeScreen()
-}
